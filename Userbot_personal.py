bb13f1f3b34778b3d1c4e4e958b673cdb9deb727
Aledev3 <99260616+Aledev3@users.noreply.github.com>
2022-03-05 21:23:17 +0100

    Update Userbot_personal.py
    up Userbot + code

--- Userbot_personal.py
+++ Userbot_personal.py
@@ -12,3 +12,20 @@ userbot_personal = TelegramClient("userbot_personal", API_ID, API_HASH)
 
 
 
+async def usbot():
+
+
+@userbot_personal.on(events.NewMessage(outgoing=True, pattern=r".status"))
+async def statususerbot(event):
+   await event.edit("userbot online")
+
+
+
+
+
+print("Userbot Personal Online!")
+
+
+with userbot_personal:
+  userbot_personal.loop.run_until_complete(usbot())
+  userbot_personal.run_until_disconnected()
