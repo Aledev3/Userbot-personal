1167c4fb4b7afe47675eb1671ff4d58119e08652
Aledev3 <99260616+Aledev3@users.noreply.github.com>
2022-05-03 21:56:41 +0200

    Update StatusUserbotOffline.py
    Fix

--- plugins_add Userbot/StatusUserbotOffline.py
+++ plugins_add Userbot/StatusUserbotOffline.py
@@ -4,6 +4,6 @@ from telethon import functions, types
 @userbot_personal.on(events.NewMessage)
 async def statusOffline(event):
   result = await client(functions.account.UpdateStatusRequest(
-        offline=False
+        offline=True
     ))
 await event.respond("I'm offline")
