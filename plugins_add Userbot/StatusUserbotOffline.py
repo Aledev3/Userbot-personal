94179b7bce260bf0031ef1adc842ddacfe116ea9
Shad0w <79542323+Shad0wGlitch@users.noreply.github.com>
2022-07-02 17:44:28 +0200

    Delete plugins_add Userbot directory

--- plugins_add Userbot/StatusUserbotOffline.py
+++
@@ -1,9 +0,0 @@
-from telethon import functions, types
-
-
-@userbot_personal.on(events.NewMessage)
-async def statusOffline(event):
-  result = await client(functions.account.UpdateStatusRequest(
-        offline=True
-    ))
-await event.respond("I'm offline")
